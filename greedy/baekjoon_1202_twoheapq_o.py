c7b5f4dd9fddb9892fb55f2422a9b1363e09096e
noah <noah@grepp.co>
2023-08-13 21:12:11 +0900

    1202문제 정답 리펙터링

--- greedy/baekjoon_1202_twoheapq_o.py
+++ greedy/baekjoon_1202_twoheapq_o.py
@@ -3,46 +3,75 @@ import sys
 import heapq
 
 
+def read_jews_in_heap_by_weight(num_jews, jews):
+    """
+    Args:
+        num_jews (int): num of jews
+        jews (list): heap of jews - ordered by weight of jew
+    """
+    for _ in range(num_jews):
+        heapq.heappush(jews, list(map(int, sys.stdin.readline().split())))
+
+
+def read_bags(num_bags, weight_of_bag):
+    """
+    Args:
+        num_bags (_type_): num of bags
+        weight_of_bag (_type_): list of bags
+    """
+    for _ in range(num_bags):
+        # weight_of_bag.append(int(input()))
+        weight_of_bag.append(int(sys.stdin.readline()))
+
+
+def find_total_cost_of_jews_in_bags(weight_of_bag, num_jews, jews_heap):
+    """_summary_
+    Args:
+        weight_of_bag (_type_): _description_
+        num_jews (_type_): _description_
+        jews_heap (_type_): _description_
+
+    Returns:
+        _type_: total_cost_of_jews
+    """
+    total_cost_of_jews = 0
+    sorted_possible_jew_heap = []
+    min_index = 0
+    for bag in weight_of_bag:
+        for _ in range(len(jews_heap)):
+            # 가방 안에 넣을 수 있는 보석을 찾기.
+            if bag >= jews_heap[0][0]:
+                # 비싼 비용 순으로 heap에 넣기
+                heapq.heappush(sorted_possible_jew_heap, -heapq.heappop(jews_heap)[1])
+            else:
+                break
+
+        if sorted_possible_jew_heap:
+            total_cost_of_jews -= heapq.heappop(sorted_possible_jew_heap)
+
+        if not sorted_possible_jew_heap and min_index == num_jews:
+            break
+
+    return total_cost_of_jews
+
+
 def main():
     """main function"""
     # input O(N+K)
     # N, K = map(int, input().split())  # list(map(int, input().split()))
     num_jews, num_bags = map(int, sys.stdin.readline().split())
-    jews = []
+    jews_heap = []
     weight_of_bag = []
-    total_cost_of_jews = 0
-    # N * LOG N
-    # for _ in range(N):
-    #    weight, cost = map(int, input().split())
-    #    heapq.heappush(jews, [weight, cost])
-    for _ in range(num_jews):
-        heapq.heappush(jews, list(map(int, sys.stdin.readline().split())))
 
-    for _ in range(num_bags):
-        # weight_of_bag.append(int(input()))
-        weight_of_bag.append(int(sys.stdin.readline()))
+    read_jews_in_heap_by_weight(num_jews, jews_heap)
+    read_bags(num_bags, weight_of_bag)
 
     # 제일 작은 가방 순
     # O(K LOG K)
     weight_of_bag.sort()
 
-    sorted_possible_jew = []
-    min_index = 0
-    for bag in weight_of_bag:
-        for _ in range(len(jews)):
-            if bag >= jews[0][0]:
-                heapq.heappush(sorted_possible_jew, -heapq.heappop(jews)[1])
-            else:
-                break
-
-        if sorted_possible_jew:
-            total_cost_of_jews -= heapq.heappop(sorted_possible_jew)
-
-        if not sorted_possible_jew and min_index == num_jews:
-            break
-
     # output
-    print(total_cost_of_jews)
+    print(find_total_cost_of_jews_in_bags(weight_of_bag, num_jews, jews_heap))
 
 
 if __name__ == "__main__":
